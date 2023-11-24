0b0812a9ade201297aadd7d14bb3ac9db09e21ee
Sivaprakash161999 <sivaprakash161999@gmail.com>
2023-11-24 23:16:54 +0530

    Forgot Password

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -145,3 +145,11 @@ MESSAGE_TAGS = {
     messages.ERROR: "danger",
     messages.WARNING: "warning",
 }
+
+# EMAIL Configuration
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+DEFAULT_FROM_EMAIL = 'foodOnline Markerplace <{}>'.format(config('EMAIL_HOST_USER'))
