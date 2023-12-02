931bdcd9cabf573e126100847a011c9504613b3a
Sivaprakash161999 <sivaprakash161999@gmail.com>
2023-12-02 23:19:28 +0530

    vendor profile form validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import User
+from .models import User, UserProfile
+from .validators import allow_only_images_validator
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -14,3 +15,29 @@ class UserForm(forms.ModelForm):
         confirm_password = cleaned_data['confirm_password']
         if password != confirm_password:
             raise forms.ValidationError("Password do not match!")
+        
+
+class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'start_typing...', 'required': 'required'}))
+    # custom validators will not work with ImageField; so changed it to FileField
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}),
+                                       validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn  btn-info'}),
+                                   validators=[allow_only_images_validator])
+    # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address', 
+                  'country', 'state', 'city', 'pin_code',
+                  'latitude', 'longitude']
+        
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
+
+                
+
+    
--- vendor/forms.py
+++ vendor/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 from .models import Vendor
+from accounts.validators import allow_only_images_validator
 
 class VendorForm(forms.ModelForm):
+    # custom validators will not work with ImageField; so changed it to FileField
+    vendor_license = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}),
+                                      validators=[allow_only_images_validator])
     class Meta:
         model = Vendor
         fields = ['vendor_name', 'vendor_license']
+        
         
